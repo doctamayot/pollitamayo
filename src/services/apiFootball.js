32adf6eb637765e2a8be567f896234eb2b2dd824
Hugo Tamayo <doctamayot@hotmail.com>
2025-09-22 17:23:26 -0500

    admonboton

--- src/services/apiFootball.js
+++ src/services/apiFootball.js
@@ -98,7 +98,6 @@ export const getStandings = async (competitionId) => {
 export const getLiveStatusesByIds = async (matchIds) => {
     if (!matchIds || matchIds.length === 0) throw new Error("El ID de competición no puede ser nulo.");
     const data = await fetchFromApi(`matches?ids=${matchIds.join(',')}`);
-    console.log(data)
     const statusMap = {};
     data.matches.forEach(match => {
         statusMap[match.id] = match.status;
