a453f7d764874726d37bba7e3d7951702e9b014e
Hugo Tamayo <doctamayot@hotmail.com>
2026-04-12 08:58:30 -0500

    cons

--- src/components/WorldCupPredictions.jsx
+++ src/components/WorldCupPredictions.jsx
@@ -13,14 +13,14 @@ import {
     extraQuestions, 
     specialEvents, 
     knockoutSubTabs 
-} from './worldcup/constants';
-import AdminPanel from './worldcup/AdminPanel';
-import StandingsTable from './worldcup/StandingsTable';
-import ExtrasTab from './worldcup/ExtrasTab';
-import EventsTab from './worldcup/EventsTab';
-import KnockoutTab from './worldcup/KnockoutTab';
-import MatchCard from './worldcup/MatchCard';
-import StatusWarnings from './worldcup/StatusWarning';
+} from './worldcupcomponents/constants';
+import AdminPanel from './worldcupcomponents/AdminPanel';
+import StandingsTable from './worldcupcomponents/StandingsTable';
+import ExtrasTab from './worldcupcomponents/ExtrasTab';
+import EventsTab from './worldcupcomponents/EventsTab';
+import KnockoutTab from './worldcupcomponents/KnockoutTab';
+import MatchCard from './worldcupcomponents/MatchCard';
+import StatusWarnings from './worldcupcomponents/StatusWarning';
 
 const WorldCupPredictions = ({ currentUser }) => {
     const isAdmin = currentUser.email === 'doctamayot@gmail.com';
